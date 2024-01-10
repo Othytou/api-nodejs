432b5f15c1350ee9ef759e5e78ac7bfb9422e1b9
Othytou <othmane@olcr.fr>
2024-01-10 20:20:31 +0100

    Add movie Info api

---
+++ routes/moviesInfo.ts
@@ -0,0 +1,26 @@
+const express = require('express');
+const request = require('request');
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = function (app) {
+    app.get('/movies/:movieName', (req, res) => {
+        const movieName = req.params.movieName;
+        const apiUrl = `https://api.themoviedb.org/3/search/movie?query=${movieName}&include_adult=false`;
+        const apiKey = process.env.SECRETKEY;
+        const headers = {
+            'Authorization': `Bearer ${apiKey}`,
+            'accept': 'application/json'
+        };
+        request.get(apiUrl, { headers }, (error, response, body) => {
+            if (error) {
+                console.error(`Code d'état de réponse non valide : ${response.statusCode}`);
+                return res.status(500).json({ error: 'Une erreur est survenue lors de la demande à l\'API TMDb.' });
+            }
+            const movieData = JSON.parse(body);
+            res.json(movieData);
+        });
+    });
+};
